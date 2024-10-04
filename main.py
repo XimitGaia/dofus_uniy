c39b00b5f8de22b090cbcc7e72bcec254fd2d9b7
Ximit <im.ximit@gmail.com>
2024-10-03 23:42:59 -0300

    segura

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import asyncio
+import time
 
 import clandestino
+import numpy as np
+import pyautogui
+from PIL import ImageGrab
 from scapy.all import sniff
 
+from src.screen.teste import detect_movement
 from src.tcp_reader.reader import TCPReader
 
 
+img1 = None
 def packet_callback(packet):
+    global img1
     try:
         _payload = bytes(packet["TCP"].payload)
         if b"MapComplementaryInformationEvent" in _payload:
-            TCPReader(bytes(packet["TCP"].payload)).read()
-        # elif b"TeleportDestinationsEvent" in _payload:
-        #     print("q")
+            # TCPReader(bytes(packet["TCP"].payload)).read()
+            time.sleep(0.75)
+            img1 = ImageGrab.grab()
+            print("lala")
+        elif b"TeleportDestinationsEvent" in _payload:
+            time.sleep(2)
+            img2 = ImageGrab.grab()
+            detect_movement(np.array(img1), np.array(img2))
+            print("q")
     except Exception as error:
         print(error)
 
--- src/screen/teste.py
+++ src/screen/teste.py
@@ -1,4 +1,6 @@
 import cv2
+import easyocr
+import pyautogui
 
 
 def load_images(image_path1, image_path2):
@@ -8,35 +10,40 @@ def load_images(image_path1, image_path2):
 
 
 def detect_movement(image1, image2):
-    # Converter para escala de cinza
     gray1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
     gray2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
 
-    # Calcular a diferença absoluta entre as imagens
     diff = cv2.absdiff(gray1, gray2)
 
-    # Aplicar um limiar para destacar as áreas com mudanças
     _, thresh = cv2.threshold(diff, 30, 255, cv2.THRESH_BINARY)
 
-    # Encontrar os contornos
     contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Desenhar caixas delimitadoras ao redor dos contornos
     for contour in contours:
-        if 2000000 > cv2.contourArea(contour) > 500000:  # Filtrar objetos pequenos
+        if cv2.contourArea(contour) > 50000:  # Filtrar objetos pequenos
             (x, y, w, h) = cv2.boundingRect(contour)
-            _img = image2.crop((x, y, x+w, y+h))
+            _img = image2[y:y+h, x:x+w]
             reader = easyocr.Reader(['en'])
 
-            results = reader.readtext(image_path)
-            cv2.rectangle(image2, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            results = reader.readtext(_img)
+            for (bbox, text, prob) in results:
+                print(text)
+                if "territory" in text.lower():
+                    print("achou")
+                    top_left = bbox[0]  # (x1, y1)
+                    bottom_right = bbox[2]  # (x3, y3)
 
+                    # Coordenadas do meio do bbox
+                    center_x = int((top_left[0] + bottom_right[0]) / 2)
+                    center_y = int((top_left[1] + bottom_right[1]) / 2)
+                    pyautogui.moveTo(x + center_x, y + center_y)
+            break
     return image2
 
 if __name__ == "__main__":
     # Especificar o caminho das imagens que você quer testar
-    image_path1 = '/Users/lucasalmeida/Desktop/base.png'  # Substitua com o caminho da sua primeira imagem
-    image_path2 = '/Users/lucasalmeida/Desktop/dif3.png'  # Substitua com o caminho da sua segunda imagem
+    image_path1 = r'C:\Users\imxim\Documents\dofus_uniy\images\base2.png'  # Substitua com o caminho da sua primeira imagem
+    image_path2 = r'C:\Users\imxim\Documents\dofus_uniy\images\dif1.png'  # Substitua com o caminho da sua segunda imagem
 
     # Carregar as imagens
     img1, img2 = load_images(image_path1, image_path2)
@@ -44,7 +51,3 @@ if __name__ == "__main__":
     # Detectar e desenhar objetos
     result_image = detect_movement(img1, img2)
 
-    # Exibir o resultado
-    cv2.imshow("Detected Movement", result_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
